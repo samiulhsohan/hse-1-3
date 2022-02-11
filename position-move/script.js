e40cd6cd4a2be03e78b4d3dcdeb45766db67a65f
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-02-11 12:40:27 +0300

    update layout

--- position-move/script.js
+++ position-move/script.js
@@ -3,14 +3,14 @@ const handleMouseMove = (e) => {
   const windowHeight = $(window).height();
   const { pageX, pageY } = e;
 
-  $(".page-x").text(pageX);
-  $(".page-y").text(pageY);
-  $(".window-size").text(`${windowWidth} x ${windowHeight}`);
+  $(".page-x").text(`x: ${pageX}`);
+  $(".page-y").text(`y: ${pageY}`);
+  $(".window-size").text(`window: ${windowWidth} x ${windowHeight}`);
 
   const saturation = Math.round((pageX / windowWidth) * 100);
   const lightness = Math.round((pageY / windowHeight) * 100);
 
-  $(".hsl").text(`hsl(235, ${saturation}%, ${lightness}%)`);
+  $(".hsl").text(`color: hsl(225, ${saturation}%, ${lightness}%)`);
   $(".box").css("background-color", `hsl(235, ${saturation}%, ${lightness}%)`);
 };
 
